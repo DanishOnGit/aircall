babfd98bc183e734225bc097781a4df4a4b79746
danishgrappus <127500304+danishgrappus@users.noreply.github.com>
2024-01-08 11:08:49 +0530

    Add tablist and tab components

---
+++ src/components/Tab/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Tab.module.css'
+const Tab = ({tabName,isActive,onTabSelect}) => {
+
+  return (
+    <div onClick={()=>onTabSelect(tabName)} className={`${styles.tab} ${isActive?styles.isActive:""}`}>
+        {tabName}
+    </div>
+  )
+}
+
+export default Tab
