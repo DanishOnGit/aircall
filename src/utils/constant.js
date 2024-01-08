430f2e2391f1661875596f8241a47e8d74ee0149
danishgrappus <127500304+danishgrappus@users.noreply.github.com>
2024-01-08 11:10:06 +0530

    Add constants and formattime util

---
+++ src/utils/constant.js
@@ -0,0 +1,15 @@
+
+export const tabs={
+  activityFeed:"Activity Feed",
+  archive:"Archive"
+}
+export const tabList = [
+  {
+    tabName: tabs.activityFeed,
+  },
+  {
+    tabName: tabs.archive,
+  },
+];
+
+export const BASE_API_URL="https://cerulean-marlin-wig.cyclic.app"
