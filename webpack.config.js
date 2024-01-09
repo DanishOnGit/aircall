b29a68821866c5b24f56c27732bd81a256892405
danishgrappus <127500304+danishgrappus@users.noreply.github.com>
2024-01-10 01:35:31 +0530

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-
+const path = require('path');
 module.exports = {
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [
       {
