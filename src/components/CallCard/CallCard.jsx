71aa735a03c817af380aa09ae8067184b6713c66
danishgrappus <127500304+danishgrappus@users.noreply.github.com>
2024-01-09 19:14:44 +0530

    Add logic to open-close modal

--- src/components/CallCard/CallCard.jsx
+++ src/components/CallCard/CallCard.jsx
@@ -1,32 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './CallCard.module.css'
-import { IncomingCall, MissedCall, OutgoingCall, Voicemail } from '../../icons/icons'
+import { IncomingCall, Information, MissedCall, OutgoingCall, Voicemail } from '../../icons/icons'
 import { formatTimestamp, getAmPm } from '../../utils/utils'
+import CallDetails from '../CallDetails/index.jsx'
+import { createPortal } from 'react-dom'
 
-const CallCard = ({ direction = 'inbound',callType='answered',from,to,createDate }) => {
+const CallCard = ({ direction = 'inbound', callType = 'answered', from, to, createDate,id }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCallId, setSelectedCallId] = useState(null);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
-  const getCallIcon=(direction,callType)=>{
-    if(!direction || !callType) throw new Error("Call direction and/or call type needed")
-    if(direction==='outbound') return <OutgoingCall/>
-    if(direction==='inbound' && callType==='answered') return <IncomingCall/>
-    if(direction==='inbound' && callType==='missed') return <MissedCall/>
-    if(direction==='inbound' && callType==='voicemail') return <Voicemail/>
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedCallId(null)
+  };
+
+  const getCallDetails=()=>{
+    openModal()
+    setSelectedCallId(id);
   }
+
+  const getCallIcon = (direction, callType) => {
+    if (!direction || !callType) throw new Error("Call direction and/or call type needed")
+    if (direction === 'outbound') return <OutgoingCall />
+    if (direction === 'inbound' && callType === 'answered') return <IncomingCall />
+    if (direction === 'inbound' && callType === 'missed') return <MissedCall />
+    if (direction === 'inbound' && callType === 'voicemail') return <Voicemail />
+  }
+
+
   return (
     <div className={styles.cardContainer}>
       <div>
-        {getCallIcon(direction,callType)}
+        {getCallIcon(direction, callType)}
       </div>
-      <div className={styles.callDetails}>
-        <p className={styles.callFrom}>{from}</p>
-        <p className={styles.callTo}>tried to call on {to}</p>
+      <div className={styles.callDetailsContainer}>
+        <div className={styles.callDetails}>
+          <p className={styles.callFrom}>{from}</p>
+          <p className={styles.callTo}>tried to call on {to}</p>
+        </div>
+        <Information onClick={getCallDetails}/>
       </div>
       <div className={styles.callTime}>
         {formatTimestamp(createDate)}
         <span className={styles.amPm}>
-        {getAmPm(createDate)}
+          {getAmPm(createDate)}
         </span>
       </div>
+      {isModalOpen && createPortal(<CallDetails selectedCallId={selectedCallId} isOpen={isModalOpen} onClose={closeModal}/>,document.getElementById("root")) }
     </div>
   )
 }
